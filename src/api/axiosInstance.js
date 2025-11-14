779e1a957e659ad9929f4276028cf67e6c4d88d9
Kushal Vikram <kushalvikram046@gmail.com>
2025-11-14 16:08:13 +0530

    Fix API base URL

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mern-backend-kpap.onrender.com/api",
+  baseURL: "https://mern-backend-kpap.onrender.com/API",
 });
 
 API.interceptors.request.use((config) => {
